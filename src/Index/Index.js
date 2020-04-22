0c406173bf207459978538e505d18ca6373f71eb
mz.jwp+1-at-785728187351 <i6zoowKCVZXrOtzq2SR8nfPYg+LIAr8Sdfe8s86TwUQ=>
2020-04-22 10:36:37 +0900

    Test

---
+++ src/Index/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Index() {
+    return (
+        <div className="Index">
+            <h1>인덱스다!</h1>
+            <h2>인덱스라고!!</h2>
+            <h3>야호야호야호</h3>
+        </div>
+    )
+}
+
+export default Index;
